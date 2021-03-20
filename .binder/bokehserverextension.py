fe89a7cef2eddd3277fead531304e5170df59476
brey <breyiannis@gmail.com>
2021-03-20 15:37:12 +0100

    Update bokehserverextension.py

--- .binder/bokehserverextension.py
+++ .binder/bokehserverextension.py
@@ -2,5 +2,5 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
-    Popen(["panel", "serve", "poseidon_viz/Thalassa.ipynb", "--allow-websocket-origin=*"])
-#   Popen(["bokeh", "serve", "poseidon_viz", "--allow-websocket-origin=*"])
+#    Popen(["panel", "serve", "poseidon_viz/Thalassa.ipynb", "--allow-websocket-origin=*"])
+    Popen(["bokeh", "serve", "poseidon_viz", "--allow-websocket-origin=*"])
