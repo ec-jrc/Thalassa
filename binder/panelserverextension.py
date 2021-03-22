4feacab01f4fa57cc0985ddca13c5bcbbb54b4b8
brey <breyiannis@gmail.com>
2021-03-22 10:01:54 +0100

    another test

--- binder/panelserverextension.py
+++ binder/panelserverextension.py
@@ -2,4 +2,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the Thalassa.ipynb with bokeh server"""
-    Popen(["panel", "serve", "--show", "Thalassa.ipynb", "--allow-websocket-origin=*"])
+    Popen(["panel", "serve", "--show", "thala.ipynb", "--allow-websocket-origin=*"])
