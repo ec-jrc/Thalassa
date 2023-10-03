4690925eaa716286839e442833dcae37943b9554
Panos Mavrogiorgos <pmav99@gmail.com>
2023-10-03 16:53:16 +0300

    docs: Add docstring to `plot()`

--- thalassa/plotting.py
+++ thalassa/plotting.py
@@ -51,7 +51,26 @@ def plot(
     y_range: tuple[float, float] | None = None,
     show_mesh: bool = False,
 ) -> gv.DynamicMap:
-    ds = normalization.normalize_dataset(ds)
+    """
+    Return the plot of the specified `variable`.
+
+    Parameters:
+        ds: The dataset which will get rendered. It must adhere to the "thalassa schema"
+        variable: The `dataset`'s variable which we want to visualize.
+        bbox: A Shapely polygon which will be used to (on-the-fly) crop the `dataset`.
+        title: The title of the plot. Defaults to `variable`.
+        cmap: The colormap to use.
+        colorbar: Boolean flag indicating whether the plot should have an integrated colorbar.
+        clabel: A caption for the colorbar. Useful for indicating e.g. units
+        clim_min: The lower limit for the colorbar.
+        clim_max: The upper limit for the colorbar.
+        x_range: A tuple indicating the minimum and maximum longitude to be displayed.
+        y_range: A tuple indicating the minimum and maximum latitude to be displayed.
+        show_mesh: A boolean flag indicating whether the mesh should be overlayed on the rendered variable.
+            Enabling this makes rendering slower.
+
+    """
+    ds = normalization.normalize(ds)
     if bbox:
         ds = utils.crop(ds, bbox)
     trimesh = api.create_trimesh(ds=ds, variable=variable)
